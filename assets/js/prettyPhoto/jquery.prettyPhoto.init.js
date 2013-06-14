3fdb7f8f30fea53f792a55bd8751f0fd9767b16f
adamKDean <adam@webhostaz.net>
2013-06-14 13:48:07 -0600

    Provide Callback functions for PrettyPhoto open and close events.
    Modified jquery.prettyPhoto.init.js to provide callback functionality to open and close events. Supports "generic" open and close events (wc_pp_close()/wc_pp_open()) or functions customized to the "type" of overlay (i.e. wc_pp_review_open()/wc_pp_photo_close()).
    
    These functions can be defined in external javascript files (such as themes or plugins) to provide extended functionality to the WooCommerce prettyPhoto overlay system.
    
    Example use cases would be to trigger analytics tracking events or dynamically setting things like a title or image description through the use of these callbacks.

--- assets/js/prettyPhoto/jquery.prettyPhoto.init.js
+++ assets/js/prettyPhoto/jquery.prettyPhoto.init.js
@@ -10,21 +10,67 @@ $(document).ready(function() {
 		theme: 'pp_woocommerce',
 		horizontal_padding: 20,
 		opacity: 0.2,
-		deeplinking: false
+		deeplinking: false,
+		changepicturecallback: function() {
+			if (typeof wc_pp_zoom_open === "function") {
+				// Launch a customized callback function first
+				wc_pp_zoom_open();
+			} else if (typeof wc_pp_open === "function") {
+				// Default to a generic "PrettyPhoto Open" callback next
+				wc_pp_open();
+			}
+		},
+		callback: function() {
+			if (typeof wc_pp_zoom_close === "function") {
+				// Launch a customized close function
+				wc_pp_zoom_close();
+			} else if (typeof wc_pp_close === function) {
+				// Default to a generic "PrettyPhoto Close" callback next
+				wc_pp_close();
+			}
+		}
 	});
 	$("a.show_review_form").prettyPhoto({
 		social_tools: false,
 		theme: 'pp_woocommerce',
 		horizontal_padding: 20,
 		opacity: 0.2,
-		deeplinking: false
+		deeplinking: false,
+		changepicturecallback: function() {
+			if (typeof wc_pp_review_open === "function") {
+				wc_pp_review_open();
+			} else if (typeof wc_pp_open === "function") {
+				wc_pp_open();
+			}
+		},
+		callback: function() {
+			if (typeof wc_pp_review_close === "function") {
+				wc_pp_review_close();
+			} else if (typeof wc_pp_close === "function") {
+				wc_pp_close();
+			}
+		}
 	});
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false,
 		theme: 'pp_woocommerce',
 		horizontal_padding: 20,
 		opacity: 0.2,
-		deeplinking: false
+		deeplinking: false,
+		changepicturecallback: function() {
+			if (typeof wc_pp_photo_open === "function") {
+				wc_pp_photo_open();
+			} else if (typeof wc_pp_open === "function") {
+				wc_pp_open();
+			}
+		},
+		callback: function() {
+			if (typeof wc_pp_photo_close === "function") {
+				wc_pp_photo_close();
+			} else if (typeof wc_pp_close === "function") {
+				wc_pp_close();
+			}
+		}
 	});
 
 	// Open review form lightbox if accessed via anchor
